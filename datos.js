97c168289cc27ce8de2033e5dfae751fec7363b9
pipe2310 <pipe.2310@hotmail.com>
2019-03-19 18:16:40 -0400

    proyecto1

---
+++ datos.js
@@ -0,0 +1,32 @@
+const opciones={
+
+	nombre:{
+		alias:'n',
+		demand: true
+	},
+	matematicas:{
+		demand: true,
+		alias: 'm'
+	},
+	ingles:{
+		demand: true,
+		alias: 'i'
+	},
+	programacion:{
+		demand: true,
+		alias: 'p'
+	}
+
+}
+
+let obtenerPromedio = (nota_uno,nota_dos,nota_tres)/3;
+
+const argv = require ('yargs')
+			.command ('promedio','Calcular el promedio',opciones);
+
+module.exports ={
+
+	obtenerPromedio,
+	argv
+
+};
